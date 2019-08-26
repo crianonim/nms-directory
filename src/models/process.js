476be1e8359d63908146e5bf077a8bd4661e5824
Jan Skowronski <crianonim@gmail.com>
2019-08-26 11:08:53 +0100

    Add refinery recipes

--- src/models/process.js
+++ src/models/process.js
@@ -1,11 +1,53 @@
-const fs=require('fs')
-const path=require('path')
+const fs = require("fs");
+const path = require("path");
 
-const resourcesFields=['Name','Abbrev','Category','Rarity','Price','Ver']
-const cont=fs.readFileSync(path.join(__dirname,'raw','resources.txt'),'utf8')
-.split('\n')
-.filter(line=>line!=='|-')
-.map(line=>line.split('||').slice(1).map(s=>s.trim().replace(/[[\]]*/g,'')))
-.filter(line=>line.length)
-.map(line=>Object.fromEntries(line.map((field,i)=>[resourcesFields[i],field])))
-fs.writeFileSync(path.join(__dirname,'resources.js'), 'export const resources = '+JSON.stringify(cont,null,1))
+const resourcesFields = [
+  "Name",
+  "Abbrev",
+  "Category",
+  "Rarity",
+  "Price",
+  "Ver"
+];
+const cont = fs
+  .readFileSync(path.join(__dirname, "raw", "resources.txt"), "utf8")
+  .split("\n")
+  .filter(line => line !== "|-")
+  .map(line =>
+    line
+      .split("||")
+      .slice(1)
+      .map(s => s.trim().replace(/[[\]]*/g, ""))
+  )
+  .filter(line => line.length)
+  .map(line =>
+    Object.fromEntries(line.map((field, i) => [resourcesFields[i], field]))
+  );
+fs.writeFileSync(
+  path.join(__dirname, "resources.js"),
+  "export const resources = " + JSON.stringify(cont, null, 1)
+);
+
+const refinery = fs
+  .readFileSync(path.join(__dirname, "raw", "refinery.csv"), "utf8")
+  .split("\n")
+  .map(line => line.split(",").slice(2))
+  .map(line => ({
+    result: { name: line[0], amount: Number(line[1]) },
+    process: { name: line[4], duration: Number(line[3]) },
+    ingredients: line.slice(5,11)
+      .reduce((prev, curr, i) => {
+          if (!curr)return prev
+        const even = i % 2 === 0;
+        if (even ) {
+            prev.push({name:curr});
+        } else {
+            prev[prev.length - 1].amount = Number(curr);
+        }
+        return prev;
+      }, [])
+  }));
+  fs.writeFileSync(
+    path.join(__dirname, "refiner.js"),
+    "export const refiner = " + JSON.stringify(refinery, null, 1)
+  );
