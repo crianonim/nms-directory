09778fda0abbb40f10cbf2681b47c042eb28b96d
Jan Skowronski <crianonim@gmail.com>
2019-08-26 11:56:39 +0100

    Add list filter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
-import {resources} from './models/resources'
+import React, { useState } from "react";
+import "./App.css";
+import { list } from "./models/model";
 function App() {
+  const {resources,refiner}=list()
+  const [nameFilter, setNameFilter] = useState("");
+  const [activeList, setActiveList] = useState(resources);
+  const [activeResource, setActiveResource] = useState(null);
   return (
     <div className="App">
-     <ul>
-       {resources.map(resource=>
-         (
-            <li key={resource.Name}>
-              {resource.Name}
-            </li>           
-         )
-       )}
-         
-     </ul>
+      <div className="resources-list">
+        <input
+          value={nameFilter}
+          onChange={({ target }) => {
+            setNameFilter(target.value);
+            setActiveList(
+              resources.filter(resource =>
+                resource.Name.toLowerCase().includes(target.value.toLowerCase())
+              )
+            );
+          }}
+        ></input>
+        <ul>
+          {activeList.map(resource => (
+            <li key={resource.Name}>{resource.Name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="resource-info">
+        {}
+      </div>
     </div>
   );
 }
