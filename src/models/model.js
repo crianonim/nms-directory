4799f9a3743a4730108618f275ec233a4fe8f1cf
Jan Skowronski <crianonim@gmail.com>
2019-08-27 10:45:45 +0100

    Add fillMissingWithEmptyData

--- src/models/model.js
+++ src/models/model.js
@@ -4,12 +4,14 @@ import {refiner} from './refiner';
 const craftables=[
     "Glass","Nanite Clusters","Di-hydrogen Jelly","Viscous Fluid"
 ]
-const items=()=> [...resources]
+
+
 
 export const list=()=>{
-   return { items:items() ,refiner,craftables}
+    return { items ,refiner,craftables}
 }
 
+let items=[...resources]
 
 
 export const recipesThatProduceInRefiner=(name)=>{
@@ -19,3 +21,31 @@ export const recipesThatProduceInRefiner=(name)=>{
 export const recipesThatUseIngredient=(name)=>{
     return refiner.filter(recipe=>recipe.ingredients.some(ing=>ing.name===name))
 }
+
+const fillMissingWithEmptyData=()=>{
+
+    let thingsMissing=[];
+    const itemNames=items.map(item=>item.name)
+    const addIfMissing=(name)=>{
+        if (!thingsMissing.includes(name) && !itemNames.includes(name)){
+            thingsMissing.push(name)
+        }
+    }
+    refiner.forEach(recipe=>{
+        addIfMissing(recipe.result.name);
+        recipe.ingredients.forEach(ing=>{
+            addIfMissing(ing.name)
+        })
+    })
+
+    thingsMissing=thingsMissing.map(thing=>({
+        name:thing,
+        price:-1,
+        category:'Unknown',
+        rarity:"Unknown"
+    }))
+    console.log(thingsMissing)
+    return thingsMissing
+}
+
+items= [...items,...fillMissingWithEmptyData()]
