2f4a1481e2531084558e24fa1acf7b256b70719f
Jan Skowronski <crianonim@gmail.com>
2019-09-01 17:47:07 +0100

    Show number of items displayed

--- src/ListFilter.js
+++ src/ListFilter.js
@@ -4,31 +4,38 @@ import "./ListFilter.css";
 const ListFilter = ({ setActiveList, items }) => {
   const [nameFilter, setNameFilter] = useState("");
   const [showFilters, setShowFilters] = useState(false);
+  const [showingCount,setShowingCount] = useState(items.length);
   const filterList = newFilter => {
     setNameFilter(newFilter);
-    setActiveList(
-      items.filter(item =>
-        item.name.toLowerCase().includes(newFilter.toLowerCase())
-      )
-    );
+    const list=items.filter(item =>
+      item.name.toLowerCase().includes(newFilter.toLowerCase()))
+    setActiveList(list);
+    setShowingCount(list.length)
   };
   return (
     <div className="list-filter">
-      <button
-        onClick={() => {
-          setShowFilters(!showFilters);
-        }}
-      >
-        {showFilters?'>':'v'}
-      </button>
-      <input
-        value={nameFilter}
-        className="list-filter-input"
-        onChange={({ target }) => {
-          filterList(target.value);
-        }}
-      ></input>
-      <button onClick={() => filterList("")}>x</button>
+      <div className="name-filter">
+        <button
+          onClick={() => {
+            setShowFilters(!showFilters);
+          }}
+        >
+          {showFilters ? ">" : "v"}
+        </button>
+        <input
+          value={nameFilter}
+          className="list-filter-input"
+          onChange={({ target }) => {
+            filterList(target.value);
+          }}
+        ></input>
+        <button onClick={() => filterList("")}>x</button>
+      </div>
+      <div className="other-filters">
+        <div>
+  Showing {showingCount} of {items.length}}
+        </div>
+      </div>
     </div>
   );
 };
