e9f7904c10e4e5d3c0a9fae3570ee7a2fccb3065
Jan Skowronski <crianonim@gmail.com>
2019-09-01 17:37:35 +0100

    Add filters show toggle

--- src/ListFilter.js
+++ src/ListFilter.js
@@ -1,27 +1,35 @@
-import React,{useState} from 'react';
-import './ListFilter.css';
+import React, { useState } from "react";
+import "./ListFilter.css";
 
-const ListFilter = ({setActiveList,items})=>{
-    const [nameFilter, setNameFilter] = useState("");
-    const filterList=(newFilter)=>{
-      setNameFilter(newFilter);
-      setActiveList(
-        items.filter(item =>
-          item.name.toLowerCase().includes(newFilter.toLowerCase())
-        )
-      );
-    }
-    return (
-        <div className="list-filter">
-        <input
-          value={nameFilter}
-          className="list-filter-input"
-          onChange={({ target }) => {
-            filterList(target.value)
-          }}
-        ></input>
-        <button onClick={()=>filterList("")}>x</button>
-        </div>
-    )
-}
-export default ListFilter
+const ListFilter = ({ setActiveList, items }) => {
+  const [nameFilter, setNameFilter] = useState("");
+  const [showFilters, setShowFilters] = useState(false);
+  const filterList = newFilter => {
+    setNameFilter(newFilter);
+    setActiveList(
+      items.filter(item =>
+        item.name.toLowerCase().includes(newFilter.toLowerCase())
+      )
+    );
+  };
+  return (
+    <div className="list-filter">
+      <button
+        onClick={() => {
+          setShowFilters(!showFilters);
+        }}
+      >
+        {showFilters?'>':'v'}
+      </button>
+      <input
+        value={nameFilter}
+        className="list-filter-input"
+        onChange={({ target }) => {
+          filterList(target.value);
+        }}
+      ></input>
+      <button onClick={() => filterList("")}>x</button>
+    </div>
+  );
+};
+export default ListFilter;
